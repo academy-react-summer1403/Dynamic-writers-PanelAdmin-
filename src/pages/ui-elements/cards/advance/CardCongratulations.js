4dfa88edc8e2be96b82496cd30ca7ab57469ccf6
KianJanloo <ddrdslayer@gmail.com>
2024-11-29 14:48:37 +0330

    Debug

--- src/pages/ui-elements/cards/advance/CardCongratulations.js
+++ src/pages/ui-elements/cards/advance/CardCongratulations.js
@@ -12,9 +12,11 @@ import decorationLeft from '@src/assets/images/elements/decore-left.png'
 import decorationRight from '@src/assets/images/elements/decore-right.png'
 import { useQuery } from '@tanstack/react-query'
 import { GetProfileAdmin } from '../../../../core/Services/api/Navbar/GetProfileAdmin'
+import { motion } from 'framer-motion';
 
 const CardCongratulations = () => {
   const {data} = useQuery({queryKey: ['GetProfileInfo'], queryFn: GetProfileAdmin})
+
 
   return (
     <Card className='card-congratulations'>
@@ -22,12 +24,13 @@ const CardCongratulations = () => {
         <img className='congratulations-img-left' src={decorationLeft} alt='decor-left' />
         <img className='congratulations-img-right' src={decorationRight} alt='decor-right' />
         <Avatar icon={<Award size={28} />} className='shadow' color='primary' size='xl' />
-        <div className='text-center'>
+        <motion.div initial={{ opacity: 0, y: -50 }}
+            whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}  className='text-center'>
           <h1 className='mb-1 text-white'>خوش آمدید, {data?.fName}</h1>
           <CardText className='m-auto w-75'>
             شما با موفقیت وارد <strong> پنل ادمین </strong> شدید, تجربه خوبی در استفاده از پنل داشته باشید!
           </CardText>
-        </div>
+        </motion.div>
       </CardBody>
     </Card>
   )
