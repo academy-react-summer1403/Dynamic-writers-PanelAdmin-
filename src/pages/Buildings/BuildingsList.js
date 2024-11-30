bf6f3ee275981661b827b14e589535967a2f2633
KianJanloo <ddrdslayer@gmail.com>
2024-11-30 20:59:28 +0330

    Debug

--- src/pages/Buildings/BuildingsList.js
+++ src/pages/Buildings/BuildingsList.js
@@ -3,16 +3,10 @@ import React from 'react'
 const BuildingsList = () => {
   return (
     <div>
-<<<<<<< HEAD
       
     </div>
   )
-=======
 
-    </div>
-  )  
-
->>>>>>> Level3
 }
 
 export default BuildingsList
