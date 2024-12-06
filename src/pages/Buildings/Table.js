f5d405fb231854ffe241632a5f90d7b58619efc6
Roshana <132509624+roshana-401@users.noreply.github.com>
2024-12-06 04:01:16 +0330

    edit building

--- src/pages/Buildings/Table.js
+++ src/pages/Buildings/Table.js
@@ -1,11 +1,12 @@
-import { Fragment, useState } from 'react'
+import { Fragment, useState, useEffect } from 'react'
 import ReactPaginate from 'react-paginate'
 import DataTable from 'react-data-table-component'
-import { Check, ChevronDown, FileText, MoreVertical, X } from 'react-feather'
+import { Check, ChevronDown, Edit, FileText, MoreVertical, X } from 'react-feather'
 import {
   Row,
   Col,
   Card,
+  Input,
   Button,
   DropdownMenu,
   DropdownItem,
@@ -14,6 +15,7 @@ import {
   Spinner,
   Badge
 } from 'reactstrap'
+import Avatar from '@components/avatar'
 import jMoment, { now } from 'jalali-moment'
 import DetailModal from './DetailModal'
 import '@styles/react/libs/react-select/_react-select.scss'
@@ -44,12 +46,8 @@ const CustomHeader = ({refetch}) => {
 }
 
 const UsersList = ({ Buildings,refetchL,isLoading}) => {
-<<<<<<< HEAD
   // ** States
   const [dataInformation, setdataInformation] = useState(null)
-=======
-
->>>>>>> b8345428de3e26d8d92154bc5b1428a6858f41be
   const [PageNumber, setPageNumber] = useState(1)
   const [Edit, setEdit] = useState(false)
   const RowsOfPage = 5
@@ -176,6 +174,7 @@ const UsersList = ({ Buildings,refetchL,isLoading}) => {
   ]
   
 
+  // ** Table data to render
   const dataToRender = () => {
     const reversedBuildings = [...Buildings].reverse()
     const start = (PageNumber - 1) * RowsOfPage
